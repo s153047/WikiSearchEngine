4a72ad802e2a43b770a896a346eaae8c4803e84a
s153047 <thomas_skafte@hotmail.com>
2018-02-11 18:49:16 +0100

    ændret test til graf

--- src/Index1.java
+++ src/Index1.java
@@ -8,7 +8,7 @@ class Index1 {
 		normal, pre, search 
 	}
 	
-	static Setting setting = Setting.normal;
+	static Setting setting = Setting.search;
     WikiItem start;
  
     private class WikiItem {
@@ -91,30 +91,56 @@ class Index1 {
     }
  
     public static void preprocessTest(String[] args){
-    	System.out.println("Preprocessing " + args[0] + " 1000 times");
+    	
     	long time, totalTime = 0;
-    	for(int j = 0; j<1000; j++){
-    		time = System.currentTimeMillis();
-            Index1 i = new Index1(args[0]);
-            totalTime += System.currentTimeMillis() - time;
+    	int numRuns = 50;
+    	int[] runTime = new int[numRuns];
+    	
+    	System.out.println("Preprocessing " + args[0]);
+    	
+    	for(int h = 1; h<numRuns; h++){
+    		totalTime = 0;
+    		for(int j = 1; j<h*h; j++){
+        		time = System.currentTimeMillis();
+                Index1 i = new Index1(args[0]);
+                runTime[h] += System.currentTimeMillis() - time;
+        	}
     	}
-        System.out.println("Preprocessing time: " + totalTime);
+    	
+        System.out.println("Preprocessing time: " );
+        for(int j = 1; j < numRuns; j++){
+        	//System.out.println(j*j + " : " + runTime[j]);
+        	System.out.println(runTime[j]);
+        }
     }
     
     public static void searchTest(String[] args){
     	long time, totalTime = 0;
+    	int numRuns = 50;
+    	int[] runTime = new int[numRuns];
+    	
     	Index1 i = new Index1(args[0]);
+    	
         totalTime = 0;
-        System.out.println("Searching 10000 times");
-        for(int j = 0; j<10000; j++){
-    		time = System.currentTimeMillis();
-    		i.search("%&/");
-            totalTime += System.currentTimeMillis() - time;
+        System.out.println("Searching: ");
+        for(int h = 1; h<numRuns; h++){
+    		totalTime = 0;
+    		for(int j = 1; j<h*h; j++){
+    			time = System.currentTimeMillis();
+        		i.search("%&/");
+                runTime[h] += System.currentTimeMillis() - time;
+        	}
     	}
-        System.out.println("Search time: " + totalTime);
+        
+        System.out.println("Search time: ");
+        for(int j = 1; j < numRuns; j++){
+        	//System.out.println(j*j + " : " + runTime[j]);
+        	System.out.println(runTime[j]);
+        }
     }
     
     public static void main(String[] args) {
+    	
     	switch(setting) {
     		case normal : 
     			normal(args);
