84c6b06a809df86da9bb3369d59ad40e806323d8
Thomas <thomas_skafte@hotmail.com>
2018-06-12 17:16:29 +0200

    hm

--- src/Index1.java
+++ src/Index1.java
@@ -11,7 +11,7 @@ class Index1 {
 		normal, pre, search, col
 	}
 	
-	static Setting setting = Setting.col;
+	static Setting setting = Setting.normal;
 	static int numRuns = 10;
 	static int numFiles =11;
 	static int startFile = 2;
@@ -123,7 +123,7 @@ class Index1 {
     
     public Index1(String filename) {
         String word;
-        
+        String[] wordArr;
         
         try {
         	Scanner input = new Scanner(new File(filename), "UTF-8");    
@@ -145,11 +145,17 @@ class Index1 {
                   }
                 
                 if(word.equals("---end.of.document---") && input.hasNext()){
-                	word = input.next().toLowerCase();
+                	input.nextLine();
+                	input.nextLine();
+                	word = input.nextLine().toLowerCase();
                     if ( (word.endsWith(",") || word.endsWith(".") || word.endsWith("?") || word.endsWith("!")) && word.length()>1) {
                     	  word = word.substring(0, word.length() - 1);
                       }
                 	document = word;
+                	wordArr = word.split(" ");
+                	for(String w : wordArr){
+                		currentHashTable.insert(w);
+                	}
                 }
                 
                 if((double) currentHashTable.n / currentHashTable.size > 1.0){
